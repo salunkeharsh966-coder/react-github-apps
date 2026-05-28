6e99ca2db074a1cf7e34b2e5fce6aaf169c339dc
Harsh Salunke <harsh@LAPTOP-9QD26SEE.(none)>
2026-05-28 15:51:33 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <h1>Welcome to my first Github Page</h1>
+      <p>Hello! ITS my very First Github Page</p>
+    </div>
+  )
+}
+
+export default App
